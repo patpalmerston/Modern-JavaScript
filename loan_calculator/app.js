2c758ed0588fa3c6df35f0a45377596884501dd2
Pat Palmerston <patpalmerston@gmail.com>
2020-01-06 09:43:40 -0800

    error function

--- loan_calculator/app.js
+++ loan_calculator/app.js
@@ -30,8 +30,35 @@ function calculateResults(e) {
             principal
         ).toFixed(2);
     } else {
-        console.log('Please check your numbers');
+        showError('Please check your numbers');
     }
 
     e.preventDefault();
+}
+
+// Show Error
+function showError(error) {
+    // Create a div
+    const errorDiv = document.createElement('div');
+
+    // Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // add class
+    errorDiv.className = 'alert alert-danger';
+
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // clear error after 3 seconds
+    setTimeout(clearError, 3000);
+}
+
+// clear error
+function clearError() {
+    document.querySelector('.alert').remove();
 }
