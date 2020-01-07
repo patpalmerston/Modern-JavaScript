8f8e98acb3bee1e60842a4bce0e125574ff7259a
Pat Palmerston <patpalmerston@gmail.com>
2020-01-06 16:11:51 -0800

    objects es five

--- objects_es5/app.js
+++ objects_es5/app.js
@@ -1,17 +1,36 @@
 // Person Constructor
 
-function Person(name, dob) {
-    this.name = name;
-    // this.age = age;
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.birthday = new Date(dob);
-    this.calculateAge = function() {
-        const diff = Date.now() - this.birthday.getTime();
-        const ageDate = new Date(diff);
-        return Math.abs(ageDate.getUTCFullYear() - 1970);
-    };
+    // this.calculateAge = function() {
+    //     const diff = Date.now() - this.birthday.getTime();
+    //     const ageDate = new Date(diff);
+    //     return Math.abs(ageDate.getUTCFullYear() - 1970);
+    // };
 }
 // console.log('outside', this);
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+};
 
-const pat = new Person('pat', '9-02-80');
+Person.prototype.calculateAge = function() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
 
+// Gets Married
+Person.prototype.getsMarried = function(newLastName) {
+    this.lastName = newLastName;
+};
+
+const pat = new Person('pat', 'palmerston', 'Sep 2 1980');
+
+console.log(pat);
 console.log(pat.calculateAge());
+pat.getsMarried('fa');
+console.log(pat.getFullName());
+console.log(pat.hasOwnProperty('firstName'));
+console.log(pat.hasOwnProperty('getFullName'));
