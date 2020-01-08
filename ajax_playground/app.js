d36c50e2d064e2afe5b99a9111395de0bac6e353
Pat Palmerston <patpalmerston@gmail.com>
2020-01-08 10:42:26 -0800

    displaying jokes on the dom

--- ajax_playground/app.js
+++ ajax_playground/app.js
@@ -10,7 +10,18 @@ function getJokes(e) {
     xhr.onload = function() {
         if (this.status === 200) {
             const response = JSON.parse(this.responseText);
-            console.log(response);
+            let output = '';
+            if (response.type === 'success') {
+                response.value.forEach(function(joke) {
+                    output += `
+                <li>${joke.joke}</li>
+                `;
+                });
+            } else {
+                output += '<li>Something went wrong</li>';
+            }
+
+            document.querySelector('.jokes').innerHTML = output;
         }
     };
 
