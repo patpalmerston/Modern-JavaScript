6ab537970db0c6df8f4b434af6aab638f331bbb4
Pat Palmerston <patpalmerston@gmail.com>
2020-01-08 10:11:04 -0800

    working with ajax and json

--- ajax_playground/app.js
+++ ajax_playground/app.js
@@ -1,49 +1,61 @@
-document.getElementById('button').addEventListener('click', loadData);
+document.getElementById('button1').addEventListener('click', loadCustomer);
 
-function loadData() {
-    // console.log('called');
-    // Creat an XHR Object
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+function loadCustomer(e) {
     const xhr = new XMLHttpRequest();
 
-    // OPEN
-    xhr.open('GET', 'data.txt', true);
-
-    // Optional - Used for spinners/loaders
-    xhr.onprogress = function() {
-        console.log('READYSTATE', xhr.readyState);
-    };
+    xhr.open('GET', 'customer.json', true);
 
     xhr.onload = function() {
-        console.log('READYSTATE', xhr.readyState);
         if (this.status === 200) {
-            document.getElementById(
-                'output'
-            ).innerHTML = `<h1>${this.responseText}</h1>`;
+            console.log(this.responseText);
+
+            const customer = JSON.parse(this.responseText);
+
+            const output = `
+            <ul>
+              <li>ID: ${customer.id} </li>
+              <li>Name: ${customer.name} </li>
+              <li>Company: ${customer.Company} </li>
+              <li>Phone: ${customer.Phone} </li>
+            </ul>
+            `;
+
+            document.getElementById('customer').innerHTML = output;
         }
     };
-
-    // xhr.onreadystatechange = function() {
-    //   console.log('READYSTATE', xhr.readyState);
-    //   if(this.status === 200 && this.readyState === 4) {
-    //     console.log(this.responseText)
-    //   }
-    // }
-
-    xhr.onerror = function() {
-        console.log('Request error...');
-    };
-
     xhr.send();
+}
 
-    // readyState Values
-    // 0: request not initialized
-    // 1: server connection established
-    // 2: request received
-    // 3: processing request
-    // 4: request finished and response is ready
+// Load Customers
 
-    // HTTP Statuses
-    // 200: "OK"
-    // 403: "Forbidden"
-    // 404: "Not Found"
+function loadCustomers(e) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'customers.json', true);
+
+    xhr.onload = function() {
+        if (this.status === 200) {
+            console.log(this.responseText);
+
+            const customers = JSON.parse(this.responseText);
+
+            let output = '';
+
+            customers.forEach(function(customer) {
+                output += `
+            <ul>
+              <li>ID: ${customer.id} </li>
+              <li>Name: ${customer.name} </li>
+              <li>Company: ${customer.Company} </li>
+              <li>Phone: ${customer.Phone} </li>
+            </ul>
+            `;
+            });
+
+            document.getElementById('customers').innerHTML = output;
+        }
+    };
+    xhr.send();
 }
