a5ee9371ddbff02f9f8853e0cdf2daf7fa146755
Pat Palmerston <patpalmerston@gmail.com>
2020-01-09 19:29:28 -0800

    posts deleted

--- easy_http/app.js
+++ easy_http/app.js
@@ -25,13 +25,37 @@ const data = {
 };
 
 // Create Post
-http.post('https://jsonplaceholder.typicode.com/posts', data, function(
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(
+//     err,
+//     post
+// ) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/3', data, function(
+//     err,
+//     post
+// ) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(
     err,
-    post
+    response
 ) {
     if (err) {
         console.log(err);
     } else {
-        console.log(post);
+        console.log(response);
     }
 });
--- easy_http/easyhttp.js
+++ easy_http/easyhttp.js
@@ -31,5 +31,29 @@ easyHttP.prototype.post = function(url, data, callback) {
 };
 
 // Make an http PUT request
+easyHttP.prototype.put = function(url, data, callback) {
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+    let self = this;
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+    };
+
+    this.http.send(JSON.stringify(data));
+};
 
 // Make an http DELETE request
+easyHttP.prototype.delete = function(url, callback) {
+    this.http.open('DELETE', url, true);
+
+    let self = this;
+    this.http.onload = function() {
+        if (self.http.status === 200) {
+            callback(null, 'post deleted');
+        } else {
+            callback('Error: ' + self.http.status);
+        }
+    };
+
+    this.http.send();
+};
