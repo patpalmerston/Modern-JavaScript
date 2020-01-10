76c5d117f0deae1e562999122ea60af747682950
Pat Palmerston <patpalmerston@gmail.com>
2020-01-10 14:44:44 -0800

    custom http library with async and await

---
+++ http_es5_es6_es7/appEs7.js
@@ -0,0 +1,28 @@
+const http = new EasyHttP();
+
+// Get User
+// http.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// User Data
+const post = {
+    name: 'pat palmerston',
+    username: 'patpalmerston',
+    email: 'patpalmerston@gmail.com'
+};
+
+// create post
+// http.post('https://jsonplaceholder.typicode.com/users', post)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// update post
+// http.put('https://jsonplaceholder.typicode.com/users/2', post)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// delete user
+http.delete('https://jsonplaceholder.typicode.com/users/1')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
