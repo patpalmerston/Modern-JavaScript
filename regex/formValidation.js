ff72f8a892b9ea06d0d5b3692ccd5c272500cb13
Pat Palmerston <patpalmerston@gmail.com>
2020-01-13 08:39:58 -0800

    validate zipcode

--- regex/formValidation.js
+++ regex/formValidation.js
@@ -17,6 +17,38 @@ function validateName() {
         name.classList.remove('is-invalid');
     }
 }
-function validateZip() {}
-function validateEmail() {}
-function validatePhone() {}
+
+function validateZip() {
+    // get value of zip field
+    const zip = document.getElementById('zip');
+    // start with 0-9, five numbers, group the dash and four number 0-9 ( but the group is optional with the "?")
+    const re = /^[0-9]{5}(-[0-9]{4})?$/;
+    // if zip does not match re then we get the alert, otherwise remove the bootstrap invalid class
+    if (!re.test(zip.value)) {
+        zip.classList.add('is-invalid');
+    } else {
+        zip.classList.remove('is-invalid');
+    }
+}
+
+// function validateEmail() {
+//   const email = document.getElementById('email');
+//   const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+//   if(!re.test(email.value)){
+//     email.classList.add('is-invalid');
+//   } else {
+//     email.classList.remove('is-invalid');
+//   }
+// }
+
+// function validatePhone() {
+//   const phone = document.getElementById('phone');
+//   const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+//   if(!re.test(phone.value)){
+//     phone.classList.add('is-invalid');
+//   } else {
+//     phone.classList.remove('is-invalid');
+//   }
+// }
