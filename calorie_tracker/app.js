71dd6a8b37ffec9b2ee5cbd4b743bd6a433fe309
Pat Palmerston <patpalmerston@gmail.com>
2020-01-14 16:36:40 -0800

    back button functional

--- calorie_tracker/app.js
+++ calorie_tracker/app.js
@@ -242,6 +242,10 @@ const App = (function(ItemCtrl, UICtrl) {
         document
             .querySelector(UISelectors.updateBtn)
             .addEventListener('click', itemUpdateSubmit);
+        // back button event
+        document
+            .querySelector(UISelectors.backBtn)
+            .addEventListener('click', UICtrl.clearEditState);
     };
 
     // Add item submit
