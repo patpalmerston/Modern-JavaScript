67600b9c2ff5a9673beeb8fde1a8440797911448
Pat Palmerston <patpalmerston@gmail.com>
2020-01-14 19:51:15 -0800

    delete working and clearing

--- calorie_tracker/app.js
+++ calorie_tracker/app.js
@@ -70,6 +70,17 @@ const ItemCtrl = (function() {
             });
             return found;
         },
+        deleteItem: function(id) {
+            // get ids
+            const ids = data.items.map(function(item) {
+                return item.id;
+            });
+            // Get index
+            const index = ids.indexOf(id);
+
+            // Remove item
+            data.items.splice(index, 1);
+        },
         setCurrentItem: function(item) {
             data.currentItem = item;
         },
@@ -169,6 +180,11 @@ const UICtrl = (function() {
                 }
             });
         },
+        deleteListItem: function(id) {
+            const itemID = `#item-${id}`;
+            const item = document.querySelector(itemID);
+            item.remove();
+        },
         clearInput: function() {
             document.querySelector(UISelectors.itemNameInput).value = '';
             document.querySelector(UISelectors.itemCaloriesInput).value = '';
@@ -246,6 +262,10 @@ const App = (function(ItemCtrl, UICtrl) {
         document
             .querySelector(UISelectors.backBtn)
             .addEventListener('click', UICtrl.clearEditState);
+        // delete item event
+        document
+            .querySelector(UISelectors.deleteBtn)
+            .addEventListener('click', itemDeleteSubmit);
     };
 
     // Add item submit
@@ -307,6 +327,27 @@ const App = (function(ItemCtrl, UICtrl) {
 
         //Updated UI
         UICtrl.updateListItem(updatedItem);
+
+        // Get total calories
+        const totalCalories = ItemCtrl.getTotalCalories();
+        // Add total calories to UI
+        UICtrl.showTotalCalories(totalCalories);
+
+        UICtrl.clearEditState();
+
+        e.preventDefault();
+    };
+
+    // Delete button event
+    const itemDeleteSubmit = function(e) {
+        // Get current item
+        const currentItem = ItemCtrl.getCurrentItem();
+
+        // Delete from data structure
+        ItemCtrl.deleteItem(currentItem.id);
+
+        // Delete from Ui
+        UICtrl.deleteListItem(currentItem.id);
 
         // Get total calories
         const totalCalories = ItemCtrl.getTotalCalories();
