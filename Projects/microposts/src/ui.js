b08000ca02c76fc69c45be099e30533c6300116e
Pat Palmerston <patpalmerston@gmail.com>
2020-01-15 11:43:34 -0800

    alert and clear fields

--- Projects/microposts/src/ui.js
+++ Projects/microposts/src/ui.js
@@ -32,6 +32,40 @@ class UI {
 
         this.post.innerHTML = output;
     }
+
+    showAlert(message, className) {
+        this.clearAlert();
+
+        // Creat div
+        const div = document.createElement('div');
+        // Add classes
+        div.className = className;
+        // Add text
+        div.appendChild(document.createTextNode(message));
+        // Get parent
+        const container = document.querySelector('.postsContainer');
+        // Get posts
+        const posts = document.querySelector('#posts');
+        // iNsert alert div
+        container.insertBefore(div, posts);
+        // Timeout
+        setTimeout(() => {
+            this.clearAlert();
+        }, 2000);
+    }
+
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+
+        if (currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    clearFields() {
+        this.titleInput.value = '';
+        this.bodyInput.value = '';
+    }
 }
 
 export const ui = new UI();
