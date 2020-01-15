81a903792f2783b4a836bfc105bd8780c41229f6
Pat Palmerston <patpalmerston@gmail.com>
2020-01-15 12:01:30 -0800

    delete functional

--- Projects/microposts/src/app.js
+++ Projects/microposts/src/app.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 //Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 function getPosts() {
     http.get('http://localhost:3000/posts')
         .then(data => ui.showPosts(data))
@@ -31,4 +34,21 @@ function submitPost() {
             getPosts();
         })
         .catch(err => console.log(err));
+}
+
+// Delete post
+function deletePost(e) {
+    e.preventDefault();
+
+    if (e.target.parentElement.classList.contains('delete')) {
+        const id = e.target.parentElement.dataset.id;
+        if (confirm('Are you sure?')) {
+            http.delete(`http://localhost:3000/posts/${id}`)
+                .then(data => {
+                    ui.showAlert('Post Removed', 'alert alert-success');
+                    getPosts();
+                })
+                .catch(err => console.log(err));
+        }
+    }
 }
